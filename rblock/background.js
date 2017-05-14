10088419745a84832be5c530b1b3a438f31ac23e
rustamspl <rustamspl@gmail.com>
2017-05-14 18:21:35 +0600

    1

--- rblock/background.js
+++ rblock/background.js
@@ -5,17 +5,33 @@ var re = (function () {
             , "adriver.ru", "mds.yandex.net", "an.yandex.ru", "mc.yandex.net"
             , "mc.yandex.ru", "stats.tmtm.ru"
             , "pagead2.googlesyndication.com"
+            , "marketgid.com"     
             , "criteo.net"
+            , "betweendigital.com"
+            , "openstat.net"
+            , "sape.ru"
+            , "acint.net"
+            , "media-rotate.com"
+            , "betsonsport.ru"
+             , "recreativ.ru"
+            , "ssl-services.com"
+            , "adlabs.ru"
+            , "dumedia.ru"
+            , "windcdna.com"
+            , "luxup.ru"
+            , "luxup2.ru"
         ].map(function (a) {
             return a.replace(/\./, '\\.');
         })
         .join('|')
-    var s = "(^[^\/]+\/\/(.*\\.)?((" + domains + ")(\\:\\d+)?\/.*)";
+    var s = "(^[^/]+//(.*\\.)?((" + domains + ")(\\:\\d+)?/.*)";
     s += "|mail\\.ru/tracker.*";
     s += "|vk\\.com/js/al/aes_light\\.js.*";
     s += "|.*(adriver|adver).*\\.js.*";
     s += "|.*google-analytics.com/(collect\\?|ga.js).*";
     s += "|.*youtube.com/api/stats/ads.*";
+    s += "|.*rutrk.org/misc/.*";
+    s += "|.*rutrk.org/\\d+.*";
     s += ")";
     return new RegExp(s, "i");
 })();
